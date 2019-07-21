53f7bc0299890a3d67ed24e8a246842a35797f33
Asus T102 H <mohamed.alhaskir@rwth-aachen.de>
2019-07-21 20:07:40 +0200

    created a project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Project-MINDS4PY',
+    version='1',
+    packages=['MINDS4PY'],
+    url='"C:/Users/Asus T102 H/PycharmProjects/Project-BIDS2MINDS',
+    license='MIT',
+    author='Mohamed Alhaskir ',
+    author_email='mohamed.alhaskir@rwth-aachen.de',
+    description='The is a MINDS schema Wrapper'
+)
